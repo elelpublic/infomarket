1520907e905fd3e02f4b7c3036ddd115058bf6f1
CreativeDream <yuliangagarin@mail.ru>
2014-11-21 15:50:17 +0100

    Update app.js

--- src/main/webapp/js/app/app.js
+++ src/main/webapp/js/app/app.js
@@ -10,7 +10,7 @@
     var app = angular.module('infomarket', ['ngRoute', 'ngSanitize']);
     var Root = '/infomarket/',
         restBase = '/infomarket/',
-        getLinkURI = Root + 'start#!/app/infomarket#/list/';
+        F = '/projectile/start#!/app/infomarket';
 
     /*
         Application Config
